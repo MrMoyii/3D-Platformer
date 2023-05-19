860180fb136fdccfdaa0966e06fd4011a32f837a
Omar Moyano <omarmoyano7w@gmail.com>
2023-05-19 12:11:48 -0300

    Add Player and playerController script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Vector3 moveDirection;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        //transform.Translate(moveDirection * Time.deltaTime * moveSpeed);
+        transform.position = transform.position + (moveDirection * Time.deltaTime * moveSpeed);
+    }
+}
